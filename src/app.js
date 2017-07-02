1f118329f065e232f3e71addfebda11951123bca
grugrut <grugruglut+github@gmail.com>
2017-07-02 15:10:13 +0900

    Using v-for.

--- src/app.js
+++ src/app.js
@@ -3,6 +3,11 @@ var Vue = require('vue');
 new Vue({
     el: '#app',
     data: {
-        text: 'Wine tasting editor'
+        text: 'Wine tasting editor',
+        clarities: [
+            { text: '澄んだ' },
+            { text: '深みのある' },
+            { text: 'やや濁った' }
+        ]
     }
 });
