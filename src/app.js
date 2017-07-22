d3c19d4feb647094b34cc8ebe220d25699b4553f
grugrut <grugruglut+github@gmail.com>
2017-07-22 22:59:28 +0900

    Add v-if.

--- src/app.js
+++ src/app.js
@@ -4,10 +4,12 @@ new Vue({
     el: '#app',
     data: {
         text: 'Wine tasting editor',
+        type: 'red',
         clarities: [
-            { text: '澄んだ' },
-            { text: '深みのある' },
-            { text: 'やや濁った' }
+            { text: '澄んだ', red: true, white: true},
+            { text: '深みのある', red: true, white: false },
+            { text: 'やや濁った', red:true, white:true },
+            { text: '濁った' , red:false, white:true}
         ]
     }
 });
